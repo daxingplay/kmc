453d45525cacacf5a95bdb9bc1392b8e4a58e1ed
daxingplay <daxingplay@gmail.com>
2012-09-27 09:35:43 +0800

    0.0.3 bugfix

---
+++ test/src/package2/init.js
@@ -0,0 +1,17 @@
+/**
+ * init
+ * @author: ×ÏÓ¢£¨daxingplay£©<daxingplay@gmail.com>
+ * @date: 12-9-26
+ * @requires: kissy 1.2+
+ */
+KISSY.add(function (S) {
+    var D = S.DOM,
+        E = S.Event,
+        LOG_PRE = '[init] ';
+
+    return {
+        init:function () {
+
+        }
+    }
+});
