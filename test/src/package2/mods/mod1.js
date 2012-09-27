370bf53a73bccd11447d1d094c88166a8a691626
daxingplay <daxingplay@gmail.com>
2012-09-27 16:52:06 +0800

    mv old test to build-test

---
+++ test/src/package2/mods/mod1.js
@@ -0,0 +1,17 @@
+/**
+ * mod1
+ * @author: ×ÏÓ¢£¨daxingplay£©<daxingplay@gmail.com>
+ * @date: 12-9-27
+ * @requires: kissy 1.2+
+ */
+KISSY.add(function (S) {
+    var D = S.DOM,
+        E = S.Event,
+        LOG_PRE = '[mod1] ';
+
+    return {
+        init:function () {
+
+        }
+    }
+});
