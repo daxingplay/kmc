a3b1f38f1056cca06c529ea86c1c9b1962d3df14
daxingplay <daxingplay@gmail.com>
2013-04-03 11:30:31 +0800

    add example

--- examples/combo.js
+++ examples/combo.js
@@ -1,6 +1,6 @@
 /**
  *
- * @author: 橘子<daxingplay@gmail.com>
+ * @author: daxingplay<daxingplay@gmail.com>
  * @time: 13-3-12 11:43
  * @description:
  */
@@ -8,26 +8,23 @@
 var kmc = require('../index'),
     fs = require('fs'),
     path = require('path'),
-    srcPath = path.resolve(__dirname, './src/'),
-    distPath = path.resolve(__dirname, './dist');
+    srcPath = path.resolve(__dirname, '../test/src/'),
+    distPath = path.resolve(__dirname, '../test/dist');
 
-var testFile = path.resolve(__dirname, './src/package2/init.js'),
-    result;
+var result;
 
-//kmc.config({
-//    packages: [{
-//        name: 'package1',
-//        path: srcPath,
-//        charset: 'gbk'
-//    }, {
-//        name: 'kissy',
-//        path: srcPath,
-//        charset: 'utf-8'
-//    }],
-//    silent: true,
-//    charset: 'gbk'
-//});
-//result = kmc.analyze(path.resolve(srcPath, 'package1/build-with-kissy.js'));
+// build with only one package.
+kmc.config({
+    packages: [{
+        name: 'package1',
+        path: srcPath,
+        charset: 'gbk'
+    }],
+    silent: true,
+    charset: 'gbk'
+});
+result = kmc.analyze(path.resolve(srcPath, 'package1/build-with-kissy.js'));
+console.log(result);
 
 var file1 = path.resolve(srcPath, 'package1/build-with-kissy.js'),
     file1output = path.resolve(distPath, 'package1/build-with-kissy.js');
